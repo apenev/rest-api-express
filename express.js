c98d6170671f33b4629eaffd99a8c1663b3a06f1
Emil Bostijancic <emil.bostijancic@bytesource.net>
2015-01-08 21:35:40 +0100

    fixed bugs

--- express.js
+++ express.js
@@ -1,83 +1,89 @@
 var express = require('express'),
-  bodyParser = require('body-parser')
-  logger = require('morgan')
+    bodyParser = require('body-parser');
+var logger = require('morgan');
+var q = require('q');
+var Promise = require('promise');
 
 var elasticSearch = require('elasticsearch');
-var ENV = require('../config.json')[GLOBAL.ENV || "dev"];
-var client = new elasticSearch.Client({
-    host: 'http://localhost:9201'
-});
+var config = require('./config.json');
 
-var searchForCollection=function (id) {
-var deferred = q.defer();
+var client = new elasticSearch.Client(config.elasticsearch);
+
+var searchForCollection = function (id) {
+
+    return new Promise(function (fulfill, reject) {
         client.get({
-            index: 'freeyup',
-            type: 'vote',
-            id: voting.voteId
-        }, function (getErr, getResp) {
-            if (getErr) {
-                votesLog.error("Error in get right Vote " , getErr);
-                deferred.reject(getErr);
-                return deferred.promise;
+            index: 'collections',
+            type: 'collection',
+            id: id
+        }, function (error, response) {
+            if (error) {
+                console.error("Error in get right Vote ", error);
+                reject(error);
             } else {
+                fulfill(response);
             }
+        });
+    });
+};
 
-
-}
-
-var app = express()
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: true}))
-app.use(logger('dev'))
-
-//var db = mongoskin.db('mongodb://@localhost:27017/test', {safe:true})
-
-app.param('collectionName', function(req, res, next, collectionName){
-  //req.collection = db.collection(collectionName)
-  return next()
-})
+var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(logger('dev'));
 
 app.get('/', function(req, res, next) {
-  res.send('please select a collection, e.g., /collections/messages')
-})
+   res.send('test');
+});
+
+app.get('/:id', function (req, res, next) {
+    var id = req.param('id', 0);
+    console.log(id);
+    searchForCollection(id).then(function (result) {
+        console.log(result);
+        res.send(result._source);
+    }, function(error) {
+        res.send(error);
+    });
+});
 
 /*
-  app.get('/collections/:collectionName', function(req, res, next) {
-  req.collection.find({} ,{limit: 10, sort: {'_id': -1}}).toArray(function(e, results){
-    if (e) return next(e)
-    res.send(results)
-  })
-})
-*/
+ app.get('/collections/:collectionName', function(req, res, next) {
+ req.collection.find({} ,{limit: 10, sort: {'_id': -1}}).toArray(function(e, results){
+ if (e) return next(e)
+ res.send(results)
+ })
+ })
+ */
 
 /* app.post('/collections/:collectionName', function(req, res, next) {
-  req.collection.insert(req.body, {}, function(e, results){
-    if (e) return next(e)
-    res.send(results)
-  })
-})
+ req.collection.insert(req.body, {}, function(e, results){
+ if (e) return next(e)
+ res.send(results)
+ })
+ })
 
-app.get('/collections/:collectionName/:id', function(req, res, next) {
-  req.collection.findById(req.params.id, function(e, result){
-    if (e) return next(e)
-    res.send(result)
-  })
-})
+ app.get('/collections/:collectionName/:id', function(req, res, next) {
+ req.collection.findById(req.params.id, function(e, result){
+ if (e) return next(e)
+ res.send(result)
+ })
+ })
 
-app.put('/collections/:collectionName/:id', function(req, res, next) {
-  req.collection.updateById(req.params.id, {$set: req.body}, {safe: true, multi: false}, function(e, result){
-    if (e) return next(e)
-    res.send((result === 1) ? {msg:'success'} : {msg: 'error'})
-  })
-})
+ app.put('/collections/:collectionName/:id', function(req, res, next) {
+ req.collection.updateById(req.params.id, {$set: req.body}, {safe: true, multi: false}, function(e, result){
+ if (e) return next(e)
+ res.send((result === 1) ? {msg:'success'} : {msg: 'error'})
+ })
+ })
 
-app.delete('/collections/:collectionName/:id', function(req, res, next) {
-  req.collection.removeById(req.params.id, function(e, result){
-    if (e) return next(e)
-    res.send((result === 1)?{msg: 'success'} : {msg: 'error'})
-  })
-})
-*/
+ app.delete('/collections/:collectionName/:id', function(req, res, next) {
+ req.collection.removeById(req.params.id, function(e, result){
+ if (e) return next(e)
+ res.send((result === 1)?{msg: 'success'} : {msg: 'error'})
+ })
+ })
+ */
 
 
 //GLOBAL.ENV = process.argv[2]||'local';
@@ -90,6 +96,10 @@ cluster = require('cluster')
 numCPUs = require('os').cpus().length;
 
 if (cluster.isMaster) {
-    for (var i = 0; i < numCPUs; i++) { cluster.fork() }
-} else { app.listen(8001); }
+    for (var i = 0; i < numCPUs; i++) {
+        cluster.fork()
+    }
+} else {
+    app.listen(8001);
+}
 
